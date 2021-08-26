6a5c0cf9d9e41cfb11419287b7460b2dc8270c92
zshakir110 <78383982+zshakir110@users.noreply.github.com>
2021-08-26 18:33:11 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+noseX=0;
+noseY=0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+  function setup() {
+  video = createCapture(VIDEO);
+  video.size(550, 500);
+
+  canvas = createCanvas(550, 550);
+  canvas.position(560,150);
+
+  poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+  console.log('PoseNet Is Initialized!');
+}
+
+
+function gotPoses(results)
+{
+  if(results.length > 0)
+  {
+    console.log(results);
+    noseX = results[0].pose.nose.x;
+    noseY = results[0].pose.nose.y;
+    console.log("noseX = " + noseX + "noseY = " + noseY);
+
+    leftWristX = results[0].pose.leftWrist.x;
+    rightWristX = results[0].pose.rightWrist.x;
+    difference = floor(leftWristX - rightWristX);
+
+    console.log("leftWrist = " + leftWristX + "rightWrist = " + rightWristX + "difference =" + difference );
+  }
+}
+
+function draw() {
+background('#0B0B45');
+  fill('#00FFFF');
+  stroke('#00FFFF');
+  square(noseX, noseY, difference);
+}
